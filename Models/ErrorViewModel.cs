8ad99c2e0a3c8f4237e7c079f08b0ab54b1b790d
Lee3060 <rahulprajapati3060@gmail.com>
2024-12-15 10:29:54 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Member_Registration_Demo_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
